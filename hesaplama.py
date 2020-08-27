ab8c83c7b33ab4451c8d266f0cdb1ddddee4c871
githsem <53379599+githsem@users.noreply.github.com>
2020-08-28 01:50:30 +0200

    Update hesaplama.py

--- hesaplama.py
+++ hesaplama.py
@@ -6,20 +6,20 @@ dekor = "*********************************"
 print("Yapmak Istediginiz Islemi Seciniz :")
 
 print(dekor)
-islem = int(print("1. Toplama\n2. Cikarma\n3. Carpma\n4. Bolme"))
+islem = int(input("1. Toplama\n2. Cikarma\n3. Carpma\n4. Bolme"))
 print(dekor)
 
 if (islem == 1):
     print("Isleminizi Sonucu : ",a+b)
 
-elif (islem == 1):
-    print("Isleminizi Sonucu : ",a+b)
+elif (islem == 2):
+    print("Isleminizi Sonucu : ",a-b)
 
-elif (islem == 1):
-    print("Isleminizi Sonucu : ",a+b)
+elif (islem == 3):
+    print("Isleminizi Sonucu : ",a*b)
 
-elif (islem == 1):
-    print("Isleminizi Sonucu : ",a+b)
+elif (islem == 4):
+    print("Isleminizi Sonucu : ",a/b)
 
 else:
     print("Gecersiz Islem")
