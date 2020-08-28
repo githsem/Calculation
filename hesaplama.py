88a3ac1618d0817164a8b85cc5a533b6bf1abeba
githsem <53379599+githsem@users.noreply.github.com>
2020-08-28 23:59:44 +0200

    update

--- hesaplama.py
+++ hesaplama.py
@@ -6,8 +6,10 @@ dekor = "*********************************"
 print("Yapmak Istediginiz Islemi Seciniz :")
 
 print(dekor)
-islem = int(input("1. Toplama\n2. Cikarma\n3. Carpma\n4. Bolme"))
+print("1. Toplama\n2. Cikarma\n3. Carpma\n4. Bolme")
 print(dekor)
+
+islem = int(input())
 
 if (islem == 1):
     print("Isleminizi Sonucu : ",a+b)
